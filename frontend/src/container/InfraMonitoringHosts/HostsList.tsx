a237c2c15d96b528d6d914d30867c088644e43dd
Vinícius Lourenço <vinicius@signoz.io>
2026-03-26 12:09:04 -0300

    fix(hosts-lists): selectedHostname was causing list query to fail to refresh when f5

--- frontend/src/container/InfraMonitoringHosts/HostsList.tsx
+++ frontend/src/container/InfraMonitoringHosts/HostsList.tsx
@@ -68,7 +68,7 @@ function HostsList(): JSX.Element {
 			...baseQuery,
 			limit: pageSize,
 			offset: (currentPage - 1) * pageSize,
-			filters,
+			filters: filters?.items?.length ? filters : undefined,
 			start: Math.floor(minTime / 1000000),
 			end: Math.floor(maxTime / 1000000),
 			orderBy,
@@ -76,15 +76,6 @@ function HostsList(): JSX.Element {
 	}, [pageSize, currentPage, filters, minTime, maxTime, orderBy]);
 
 	const queryKey = useMemo(() => {
-		if (selectedHostName) {
-			return [
-				'hostList',
-				String(pageSize),
-				String(currentPage),
-				JSON.stringify(filters),
-				JSON.stringify(orderBy),
-			];
-		}
 		return [
 			'hostList',
 			String(pageSize),
@@ -94,15 +85,7 @@ function HostsList(): JSX.Element {
 			String(minTime),
 			String(maxTime),
 		];
-	}, [
-		pageSize,
-		currentPage,
-		filters,
-		orderBy,
-		selectedHostName,
-		minTime,
-		maxTime,
-	]);
+	}, [pageSize, currentPage, filters, orderBy, minTime, maxTime]);
 
 	const { data, isFetching, isLoading, isError } = useGetHostList(
 		query as HostListPayload,
